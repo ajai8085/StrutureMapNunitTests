1e4d5f1a9e60344d1d9959ae0e1623606defae7a
Ajai <usr.registered@gmail.com>
2014-10-18 13:02:15 +0800

    initial commit

---
+++ LibToTest/AppServices.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibToTest
+{
+    public class AppServices
+    {
+      public AppServices()
+      {
+
+      }
+
+      public int SaveData()
+      {
+        Console.WriteLine("Saved");
+        return 100;
+      }
+    }
+}
---
+++ StrutureMapNunitTests/DefaultTest.cs
@@ -0,0 +1,17 @@
+﻿using LibToTest;
+using NUnit.Framework;
+using StructureMap.AutoMocking;
+
+namespace StrutureMapNunitTests
+{
+  [TestFixture]
+  public class DefaultTest
+  {
+    [TestCase]
+    public void TestOne()
+    {
+      var auto = new RhinoAutoMocker<AppServices>();
+      var item =auto.ClassUnderTest;
+    }
+  }
+}
